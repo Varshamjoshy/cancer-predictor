94ee7e81aa04f0fe97b379dbc2552c99bd07042a
AntonyNeerajJPp <antonyneeraj.jp@gmail.com>
2024-09-07 18:34:49 +0530

    This is the first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+  input_features = [int(x) for x in request.form.values()]
+  features_value = [np.array(input_features)]
+
+  features_name = ['radius_mean', 'texture_mean', 'perimeter_mean',
+       'area_mean', 'smoothness_mean', 'compactness_mean',
+       'concavity_mean', 'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean']
+
+  df = pd.DataFrame(features_value, columns=features_name)
+  output = model.predict(df)
+
+  if int(output) == 1:
+      res_val = "Breast cancer"
+  else:
+      res_val = "no Breast cancer"
+
+
+  return render_template('index.html', prediction_text='Patient has {}'.format(res_val))
+
+if __name__ == "__main__":
+  app.run()
